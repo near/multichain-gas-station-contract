fe1a4447ccaa9aeb5e015025a8bd01e51dd3377f
Jacob Lindahl <encody@users.noreply.github.com>
2024-03-14 20:32:15 +0900

    feat: full nft key test

--- mock/nft_key/tests/nft_key.rs
+++ mock/nft_key/tests/nft_key.rs
@@ -1,20 +1,27 @@
 use near_sdk::serde_json::json;
+use near_workspaces::types::NearToken;
 
 #[tokio::test]
-async fn test_workflow_happy_path() {
+async fn test_nft_key() {
     let w = near_workspaces::sandbox().await.unwrap();
 
-    let (nft_key, signer) = tokio::join!(
+    let (nft_key, signer, alice, bob) = tokio::join!(
         async {
-            let wasm = near_workspaces::compile_project("./").await.unwrap();
-            w.dev_deploy(&wasm).await.unwrap()
+            w.dev_deploy(&near_workspaces::compile_project("./").await.unwrap())
+                .await
+                .unwrap()
         },
         async {
-            let wasm = near_workspaces::compile_project("../signer").await.unwrap();
-            w.dev_deploy(&wasm).await.unwrap()
+            w.dev_deploy(&near_workspaces::compile_project("../signer").await.unwrap())
+                .await
+                .unwrap()
         },
+        async { w.dev_create_account().await.unwrap() },
+        async { w.dev_create_account().await.unwrap() },
     );
 
+    println!("{:<16} {}", "Alice:", alice.id());
+    println!("{:<16} {}", "Bob:", bob.id());
     println!("{:<16} {}", "NFT Key:", nft_key.id());
     println!("{:<16} {}", "Signer:", signer.id());
 
@@ -34,8 +41,8 @@ async fn test_workflow_happy_path() {
 
     let (token_1_id, token_2_id) = tokio::join!(
         async {
-            nft_key
-                .call("mint")
+            alice
+                .call(nft_key.id(), "mint")
                 .args_json(json!({}))
                 .transact()
                 .await
@@ -44,8 +51,8 @@ async fn test_workflow_happy_path() {
                 .unwrap()
         },
         async {
-            nft_key
-                .call("mint")
+            alice
+                .call(nft_key.id(), "mint")
                 .args_json(json!({}))
                 .transact()
                 .await
@@ -58,10 +65,10 @@ async fn test_workflow_happy_path() {
     let msg_1 = [1u8; 32];
     let msg_2 = [2u8; 32];
 
-    let (sig_1, sig_2) = tokio::join!(
+    let (alice_success, bob_fail) = tokio::join!(
         async {
-            nft_key
-                .call("ck_sign_hash")
+            alice
+                .call(nft_key.id(), "ck_sign_hash")
                 .args_json(json!({
                     "path": token_1_id,
                     "payload": msg_1,
@@ -74,8 +81,7 @@ async fn test_workflow_happy_path() {
                 .unwrap()
         },
         async {
-            nft_key
-                .call("ck_sign_hash")
+            bob.call(nft_key.id(), "ck_sign_hash")
                 .args_json(json!({
                     "path": token_2_id,
                     "payload": msg_2,
@@ -84,11 +90,46 @@ async fn test_workflow_happy_path() {
                 .transact()
                 .await
                 .unwrap()
-                .json::<String>()
-                .unwrap()
         },
     );
 
-    println!("sig1: {sig_1:?}");
-    println!("sig2: {sig_2:?}");
+    println!("Alice successfully signed: {alice_success}");
+
+    assert!(
+        bob_fail.is_failure(),
+        "Bob is unauthorized to sign with an NFT he does not own"
+    );
+
+    println!("Transferring token {token_2_id} to Bob...");
+
+    alice
+        .call(nft_key.id(), "nft_transfer")
+        .args_json(json!({
+            "token_id": token_2_id,
+            "receiver_id": bob.id(),
+        }))
+        .deposit(NearToken::from_yoctonear(1))
+        .max_gas()
+        .transact()
+        .await
+        .unwrap()
+        .unwrap();
+
+    println!("Token transferred.");
+    println!("Bob attempting to sign with token {token_2_id}...");
+
+    let bob_success = bob
+        .call(nft_key.id(), "ck_sign_hash")
+        .args_json(json!({
+            "path": token_2_id,
+            "payload": msg_2,
+        }))
+        .max_gas()
+        .transact()
+        .await
+        .unwrap()
+        .json::<String>()
+        .unwrap();
+
+    println!("After transfer, Bob signed: {bob_success}");
 }
