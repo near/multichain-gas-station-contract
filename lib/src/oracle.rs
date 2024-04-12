71a65587150589ad7edbf309d47226eb04762812
Jacob Lindahl <encody@users.noreply.github.com>
2024-04-12 22:16:52 +0900

    chore: remove old priceoracle.near code

--- lib/src/oracle.rs
+++ lib/src/oracle.rs
@@ -1,43 +1,9 @@
-use near_sdk::{
-    borsh::{self, BorshDeserialize, BorshSerialize},
-    bs58, ext_contract,
-    json_types::{U128, U64},
-    serde::{Deserialize, Serialize},
-};
-use schemars::JsonSchema;
+use near_sdk::bs58;
 
 use crate::Rejectable;
 
 pub const PYTH_PRICE_ID_NEAR_USD: &str = "3gnSbT7bhoTdGkFVZc1dW1PvjreWzpUNUD5ppXwv1N59";
 pub const PYTH_PRICE_ID_ETH_USD: &str = "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw";
-
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, JsonSchema, Clone, Copy)]
-#[serde(crate = "near_sdk::serde")]
-pub struct Price {
-    pub multiplier: U128,
-    pub decimals: u8,
-}
-
-#[derive(Serialize, Deserialize, JsonSchema)]
-#[serde(crate = "near_sdk::serde")]
-pub struct AssetOptionalPrice {
-    pub asset_id: String,
-    pub price: Option<Price>,
-}
-
-#[derive(Serialize, Deserialize, JsonSchema)]
-#[serde(crate = "near_sdk::serde")]
-pub struct PriceData {
-    pub timestamp: U64,
-    pub recency_duration_sec: u32,
-
-    pub prices: Vec<AssetOptionalPrice>,
-}
-
-#[ext_contract(ext_oracle)]
-pub trait OracleInterface {
-    fn get_price_data(&self, asset_ids: Option<Vec<String>>) -> PriceData;
-}
 
 pub fn decode_pyth_price_id(s: &str) -> [u8; 32] {
     let mut b = [0u8; 32];
--- mock/oracle/src/lib.rs
+++ mock/oracle/src/lib.rs
@@ -1,10 +1,7 @@
-use lib::oracle::{
-    decode_pyth_price_id, AssetOptionalPrice, OracleInterface, Price, PriceData,
-    PYTH_PRICE_ID_ETH_USD, PYTH_PRICE_ID_NEAR_USD,
-};
+use lib::oracle::{decode_pyth_price_id, PYTH_PRICE_ID_ETH_USD, PYTH_PRICE_ID_NEAR_USD};
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
-    env, near_bindgen,
+    near_bindgen,
 };
 
 #[derive(BorshSerialize, BorshDeserialize, Default, Debug)]
@@ -36,57 +33,6 @@ impl Contract {
             })
         } else {
             None
-        }
-    }
-}
-
-#[near_bindgen]
-impl OracleInterface for Contract {
-    fn get_price_data(&self, asset_ids: Option<Vec<String>>) -> PriceData {
-        let [a, b]: [_; 2] = asset_ids.unwrap().try_into().unwrap();
-
-        match (a.as_str(), b.as_str()) {
-            ("wrap.testnet", "weth.fakes.testnet") => PriceData {
-                prices: vec![
-                    AssetOptionalPrice {
-                        asset_id: "wrap.testnet".to_string(),
-                        price: Some(Price {
-                            decimals: 28,
-                            multiplier: 28770.into(),
-                        }),
-                    },
-                    AssetOptionalPrice {
-                        asset_id: "weth.fakes.testnet".to_string(),
-                        price: Some(Price {
-                            decimals: 20,
-                            multiplier: 226_435.into(),
-                        }),
-                    },
-                ],
-                recency_duration_sec: 90,
-                timestamp: env::block_timestamp().into(),
-            },
-            ("local_ft.testnet", "weth.fakes.testnet") => PriceData {
-                prices: vec![
-                    AssetOptionalPrice {
-                        asset_id: "local_ft.testnet".to_string(),
-                        price: Some(Price {
-                            decimals: 28,
-                            multiplier: 28770.into(),
-                        }),
-                    },
-                    AssetOptionalPrice {
-                        asset_id: "weth.fakes.testnet".to_string(),
-                        price: Some(Price {
-                            decimals: 20,
-                            multiplier: 226_435.into(),
-                        }),
-                    },
-                ],
-                recency_duration_sec: 90,
-                timestamp: env::block_timestamp().into(),
-            },
-            _ => env::panic_str("Unknown asset pair"),
         }
     }
 }
